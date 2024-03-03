8102324cae263a1cb3f117d90e824601fc214c2d
Hugo.D <hugo.divet@epsi.fr>
2024-03-03 02:47:03 +0100

    Collisions weapon / boss

--- Assets/Scripts/Boss/BossMain.cs
+++ Assets/Scripts/Boss/BossMain.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstBoss : MonoBehaviour
+public class BossMain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public BossStats bossStats;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Awake()
     {
-    }   
+        SendMessage("Init", this);
+    }
 
 }
 
