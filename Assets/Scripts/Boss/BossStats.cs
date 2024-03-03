700e979ee0ce3dd9682ea085fe61dfcb6af1c14b
Hugo.D <hugo.divet@epsi.fr>
2024-03-03 03:44:01 +0100

    collisions done

--- Assets/Scripts/Boss/BossStats.cs
+++ Assets/Scripts/Boss/BossStats.cs
@@ -9,10 +9,12 @@ public class BossStats : MonoBehaviour
     private float damage;
     private float defense;
     public BossScriptable scriptable;
+    private BossMain bossMain;
 
     public void Init(BossMain main)
     {
         main.bossStats = this;
+        bossMain = main;
     }
 
     void Awake()
@@ -21,5 +23,19 @@ public class BossStats : MonoBehaviour
         currentHP = scriptable.maxHP;
         damage = scriptable.damage;
         defense = scriptable.defense;
+    }
+
+    private void Start()
+    {
+        bossMain.bossCollision.damage += TakeDamage;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        bossMain.bossStats.currentHP -= damage;
+        if(bossMain.bossStats.currentHP < 0)
+        {
+            Debug.Log("Aller hop le boss");
+        }
     }
 }
